1c7d5d5fb6cad241a39099925848879e9a660867
Zeljko Tomic <scyth@unixreich.com>
2014-12-09 04:18:35 +0100

    too late to keep going

---
+++ transport_local.go
@@ -0,0 +1,4 @@
+package dentrite
+
+// Local Transport is used when reaching out to local vnodes
+type LocalTransport struct{}
---
+++ vnode.go
@@ -0,0 +1,4 @@
+package dentrite
+
+// VNode structure
+type Vnode struct{}
